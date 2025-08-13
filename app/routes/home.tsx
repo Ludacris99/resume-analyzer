e258d81b8d8dfd298b11224545076a381361ce30
Paavan <paavanpatni99@gmail.com>
2025-08-14 00:50:08 +0530

    home page done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "JobFit" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading ">
+        <h1>Discover Powerful Insights In Your Resume With AI</h1>
+        <h2>Track your applications & resume ratings</h2>
+      </div>
+
+      {resumes.length > 0 && (
+
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+
+        </div>
+
+      )}
+
+    </section>
+  </main>;
 }
